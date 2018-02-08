a44ccbd8066d123fbdd5328726b9ad9702354695
Simon Wyckaert <simon@digitold.com>
2018-02-08 23:20:25 +0100

    un peu de mise en forme

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,10 @@ class Event < ApplicationRecord
 	has_many :registrations
 	has_many :users, through: :registrations
 
+	before_save { self.title = title.titleize }
+  before_save { self.location = location.titleize }
+  validates :title, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :date, presence: true
+  validates :location, presence: true, length: { minimum: 2, maximum: 100 }
+
 end
