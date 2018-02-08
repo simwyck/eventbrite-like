b2175403a0c94d3fc212eef38062aec1ea127e20
Simon Wyckaert <simon@digitold.com>
2018-02-08 12:03:20 +0100

    MAJ migrations + db migrate

--- db/migrate/20180208101346_create_registrations.rb
+++ db/migrate/20180208101346_create_registrations.rb
@@ -1,7 +1,8 @@
 class CreateRegistrations < ActiveRecord::Migration[5.1]
   def change
     create_table :registrations do |t|
-
+      t.belongs_to :event, index: true
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
--- db/migrate/20180208101353_create_creations.rb
+++ db/migrate/20180208101353_create_creations.rb
@@ -1,7 +1,8 @@
 class CreateCreations < ActiveRecord::Migration[5.1]
   def change
     create_table :creations do |t|
-
+      t.belongs_to :event, index: true
+      t.belongs_to :user, index: true
       t.timestamps
     end
   end
